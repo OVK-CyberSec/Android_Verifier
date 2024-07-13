5e8e2a2d8a23f4d363c0ec91887bbd6d1dfe25f7
Amine Hifdi <101675260+OVK-CyberSec@users.noreply.github.com>
2024-07-13 03:33:45 +0100

    Update police_style.py

--- police_style.py
+++ police_style.py
@@ -8,11 +8,15 @@ import shutil
 
 import time
 
+import itertools
 
+import sys
 
 Red = "\033[31m"
 
 Green = "\033[32m"
+
+Purple = "\033[35m"
 
 Yellow = "\033[33m"
 
@@ -58,21 +62,11 @@ def center_text(text):
 
 
 
-def loading_animation():
-
-    import itertools
-
-    import time
-
-    import sys
-
-
+def load_animation():
 
     spinner = itertools.cycle(['-', '\\', '|', '/'])
 
     loading_text = "Loading"
-
-
 
     for _ in range(10):  # Adjust the number of iterations as needed
 
@@ -80,14 +74,49 @@ def loading_animation():
 
         sys.stdout.flush()
 
-        time.sleep(0.1)
-
-
+        time.sleep(0.3)
 
     sys.stdout.write('\r' + ' ' * shutil.get_terminal_size().columns)
 
     sys.stdout.flush()
 
+
+def decomp_animation():
+
+    spinner = itertools.cycle(['-', '\\', '|', '/'])
+
+    loading_text = "Decompilation in progress ..."
+
+    for _ in range(10):  # Adjust the number of iterations as needed
+
+        sys.stdout.write(f"\r{center_text(f'[{next(spinner)}] {loading_text}')}")
+
+        sys.stdout.flush()
+
+        time.sleep(0.3)
+
+    sys.stdout.write('\r' + ' ' * shutil.get_terminal_size().columns)
+
+    sys.stdout.flush()
+
+
+def rep_animation():
+
+    spinner = itertools.cycle(['-', '\\', '|', '/'])
+
+    loading_text = "Generating Report ..."
+
+    for _ in range(10):  # Adjust the number of iterations as needed
+
+        sys.stdout.write(f"\r{center_text(f'[{next(spinner)}] {loading_text}')}")
+
+        sys.stdout.flush()
+
+        time.sleep(0.3)
+
+    sys.stdout.write('\r' + ' ' * shutil.get_terminal_size().columns)
+
+    sys.stdout.flush()
 
 
 def simulate_long_task():
@@ -104,68 +133,92 @@ def clear_screen():
 
 def choice_manifest():
 
-    print(f"\n{Cyan}{bold}Menu :{reset}")
+    print(f"{Cyan}{bold}Menu :{reset}")
 
     print("\n")
 
-    print(f"{bold}{Red} ====================================================================================={reset}")
+    print(f"{bold}{Red} ==============================================================={reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}Options{reset}                {Red}|{reset}              {Green}{bold}Description{reset}                                   {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}Options{reset}   {Red}|{reset}              {Green}{bold}Description{reset}                          {Red}|{reset}")
 
-    print(f"{bold}{Red} ====================================================================================={reset}")
+    print(f"{bold}{Red} ==============================================================={reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}1{reset}                     {Red}|{reset}       {Green}{bold}Checking for Application Attributes...{reset}                {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}1{reset}         {Red}|{reset}       {Green}{bold}Checking for Application Attributes...{reset}      {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}2{reset}                     {Red}|{reset}       {Green}{bold}Checking for Permission...{reset}                            {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}2{reset}         {Red}|{reset}       {Green}{bold}Checking for Permission...{reset}                  {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}3{reset}                     {Red}|{reset}       {Green}{bold}Checking for Exported Component...{reset}                    {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}3{reset}         {Red}|{reset}       {Green}{bold}Checking for Exported Component...{reset}          {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}4{reset}                     {Red}|{reset}       {Green}{bold}Checking for SDK version...{reset}                           {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}4{reset}         {Red}|{reset}       {Green}{bold}Checking for SDK version...{reset}                 {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}5{reset}                     {Red}|{reset}       {Green}{bold}Checking for DeepLinks...{reset}                             {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}5{reset}         {Red}|{reset}       {Green}{bold}Checking for DeepLinks...{reset}                   {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}6{reset}                     {Red}|{reset}       {Green}{bold}Returning to previous menu...{reset}                         {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}6{reset}         {Red}|{reset}       {Green}{bold}Returning to previous menu...{reset}               {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}q{reset}                     {Red}|{reset}       {Green}{bold}Quit{reset}                                                  {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}q{reset}         {Red}|{reset}       {Green}{bold}Quit{reset}                                        {Red}|{reset}")
 
-    print(f"{bold}{Red} ====================================================================================={reset}")
-
-    print("\n")
-
-
-
-def choice_menu():
+    print(f"{bold}{Red} ==============================================================={reset}")
 
     print("\n")
+
+
+
+def choice_meta():
 
     print(f"{Cyan}{bold}Menu :{reset}")
 
     print("\n")
 
-    print(f"{bold}{Red} ============================================================================={reset}")
+    print(f"{bold}{Red} ================================================================={reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}Options{reset}               {Red}|{reset}             {Green}{bold}Description{reset}                             {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}Options{reset}  {Red}|{reset}          {Green}{bold}Description{reset}                                {Red}|{reset}")
 
-    print(f"{bold}{Red} ============================================================================={reset}")
+    print(f"{bold}{Red} ================================================================={reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}1{reset}                     {Red}|{reset}       {Green}{bold}Checking for Code Obfuscation...{reset}              {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}1{reset}        {Red}|{reset}       {Green}{bold}Checking for MANIFEST.MF...{reset}                   {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}2{reset}                     {Red}|{reset}       {Green}{bold}Checking for AndroidManifest File...{reset}          {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}2{reset}        {Red}|{reset}       {Green}{bold}Checking for CERT.SF...{reset}                       {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}3{reset}                     {Red}|{reset}       {Green}{bold}Generate Full Report...{reset}                       {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}3{reset}        {Red}|{reset}       {Green}{bold}Checking for CERT.RSA...{reset}                      {Red}|{reset}")
 
-    print(f"{bold}{Red}| {Green}{bold}q{reset}                     {Red}|{reset}       {Green}{bold}Quit{reset}                                          {Red}|{reset}")
+    print(f"{bold}{Red}| {Green}{bold}4{reset}        {Red}|{reset}       {Green}{bold}Returning to previous menu...{reset}                 {Red}|{reset}")
 
-    print(f"{bold}{Red} ============================================================================={reset}")
+    print(f"{bold}{Red}| {Green}{bold}q{reset}        {Red}|{reset}       {Green}{bold}Quit{reset}                                          {Red}|{reset}")
+
+    print(f"{bold}{Red} ================================================================={reset}")
 
     print("\n")
 
+
+
+
+def choice_menu():
+
+
+    print(f"{Cyan}{bold}Menu :{reset}")
+
+    print("\n")
+
+    print(f"{bold}{Red} ==========================================================================={reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}Options{reset}  {Red}|{reset}          {Green}{bold}Description{reset}                                          {Red}|{reset}")
+
+    print(f"{bold}{Red} ==========================================================================={reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}1{reset}        {Red}|{reset}     {Green}{bold}Checking Overview [FILES/ACTIVITIES/SERVICES/etc...]{reset}      {Red}|{reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}2{reset}        {Red}|{reset}     {Green}{bold}Checking for Code Obfuscation...{reset}                          {Red}|{reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}3{reset}        {Red}|{reset}     {Green}{bold}Checking for AndroidManifest File...{reset}                      {Red}|{reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}4{reset}        {Red}|{reset}     {Green}{bold}Checking for META-INT File...{reset}                             {Red}|{reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}5{reset}        {Red}|{reset}     {Green}{bold}Generate Full Report...{reset}                                   {Red}|{reset}")
+
+    print(f"{bold}{Red}| {Green}{bold}q{reset}        {Red}|{reset}     {Green}{bold}Quit{reset}                                                      {Red}|{reset}")
+
+    print(f"{bold}{Red} =========================================================================={reset}")
+
     print("\n")
 
 
-
-if __name__ == "__main__":
-
-    # This file is meant to be imported, not run directly
-
-    pass
